5db332f92c111cc14b25a85439e93da6bc959786
jhnnsrs <jhnnsrs@gmail.com>
2023-10-19 18:56:17 +0200

    again

--- build-windows.py
+++ build-windows.py
@@ -1,18 +1,20 @@
 import PyInstaller.__main__
-import shutil 
+import shutil
 
 
 APP_NAME = "MikroManager"
 
-PyInstaller.__main__.run([
-    'entrypoint.py',
-    '--clean',
-    '--windowed',
-    "--onedir",
-    f'--name={APP_NAME}',
-    '--noconfirm',
-    '--additional-hooks-dir=hooks',
-])
+PyInstaller.__main__.run(
+    [
+        "entrypoint.py",
+        "--clean",
+        "--windowed",
+        "--onedir",
+        f"--name={APP_NAME}",
+        "--noconfirm",
+        "--additional-hooks-dir=hooks",
+    ]
+)
 
 shutil.make_archive("./dist/MikroManager", "zip", "./dist/MikroManager")
 print("Made archive")
