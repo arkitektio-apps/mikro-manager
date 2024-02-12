5d870ee1ab9ac25621a9e2e905c8869521eccc36
jhnnsrs <jhnnsrs@gmail.com>
2024-02-12 17:28:59 +0100

    fixed tests

--- tests/test_no_interference.py
+++ tests/test_no_interference.py
@@ -1,8 +1,8 @@
-from mikro_manager.main import MMBridge
+from mikro_manager.main import MikroManager
 import pytest
 
 
 @pytest.mark.qt
 def test_fetch_from_windowed_grant(qtbot):
-    widget = MMBridge()
+    widget = MikroManager()
     qtbot.addWidget(widget)
